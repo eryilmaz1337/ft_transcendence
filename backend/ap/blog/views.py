dd24d9a378515efa3e9b56cffe2d8120d9f31da2
eryilmaz1337 <eryilmaz.1337@gmail.com>
2024-02-01 07:54:06 +0300

    docker file volume add

--- backend/ap/blog/views.py
+++
@@ -1,7 +0,0 @@
-from django.http.response import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-
-def index(request):
-    return render(request, "index.html")
--- backend/app/mytest/wsgi.py
+++ backend/app/mytest/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for blogapp project.
+WSGI config for mytest project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mytest.settings')
 
 application = get_wsgi_application()
