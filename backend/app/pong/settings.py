361f04af79b9b673c07d209fff1f02e034318399
burakkozluca <burak_kozluca@hotmail.com>
2024-02-26 18:48:29 +0300

    chat

--- backend/app/pong/settings.py
+++ backend/app/pong/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
+    'chat',
     'topla',
     'corsheaders',
     'django.contrib.admin',
@@ -112,6 +114,15 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'pong.wsgi.application'
+ASGI_APPLICATION = 'pong.asgi.application'
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
--- backend/app/pong/urls.py
+++ backend/app/pong/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path , include
 
 urlpatterns = [
+    path("chat/", include("chat.urls")),
     path('admin/', admin.site.urls),
     path('api/', include('topla.urls')),
 ]
