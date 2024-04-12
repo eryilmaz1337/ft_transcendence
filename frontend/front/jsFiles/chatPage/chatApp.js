a402259cd891d6dbe7cf7347c113a64730bd31f6
mmtemel <mmteml41@gmail.com>
2024-04-12 18:51:46 +0300

    chat drowpdown, colors will be added and developed

--- frontend/front/jsFiles/chatPage/chatApp.js
+++ frontend/front/jsFiles/chatPage/chatApp.js
@@ -17,8 +17,8 @@ function userchanges(name)
 {
     const chatInput = document.getElementById('chat-text');
     const chatHeader = document.getElementById('chat-header');
-    chatHeader.innerText = `${name} chatting...`;
-    chatInput.placeholder = `Type here, ${name}...`;
+    chatHeader.innerText = `${name}`;
+    chatInput.placeholder = `Type to ${name}...`;
     //clearmessage();
 }
 
